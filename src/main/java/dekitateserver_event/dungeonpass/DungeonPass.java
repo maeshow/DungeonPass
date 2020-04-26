b6f604ec72ecfde08652bffd52f6b2629c395be3
maeshow <necobudou3011@gmail.com>
2020-04-26 23:10:41 +0900

    first commit

---
+++ src/main/java/dekitateserver_event/dungeonpass/DungeonPass.java
@@ -0,0 +1,17 @@
+package dekitateserver_event.dungeonpass;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DungeonPass extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
